87fd1b5f83edb1071e289d86fa92a309846e343a
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-05-18 14:27:41 +0000

    IN PROGRESS

---
+++ OnlineOrderSystem/MenuItem.java
@@ -0,0 +1,26 @@
+package OnlineOrderSystem;
+
+public class MenuItem{
+    private String name;
+    private double price;
+
+    public MenuItem(String name, double price){
+        this.name=name;
+        this.price=price;
+    }   public String getName(){
+        return name;
+    }   public double getPrice(){
+        return price;
+    }   public void setName(String name){
+        this.name=name;
+    }   public void setPrice(double price){
+        if(price <= 0){
+            this.price=0;
+        }else{
+            this.price=price;
+        }
+    }   public double getTotalPrice(){
+        return price;
+
+    }
+}
