6f9f8de27272804940a20e42e0979862c7e4e599
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-05-27 13:52:31 +0000

    IN PROGRESS

---
+++ practice/practice1/Vehicles.java
@@ -0,0 +1,18 @@
+package practice.practice1;
+
+public abstract class Vehicles {
+private String vehicleName;
+
+public Vehicles(String vehicleName{
+    this.vehicleName=vehicleName;
+}   public String getVehicleName(){
+    return vehicleName;
+}   public void setVehicleName(String vehicleName){
+    this.vehicleName=vehicleName;
+}   
+
+
+ public abstract double fare(){
+
+ }   
+}
