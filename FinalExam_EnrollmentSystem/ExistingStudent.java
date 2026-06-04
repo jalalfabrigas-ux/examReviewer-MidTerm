dcce940443959b14afdf2e975a48709e624b6976
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-06-04 12:16:42 +0000

    constructor

--- FinalExam_EnrollmentSystem/ExistingStudent.java
+++ FinalExam_EnrollmentSystem/ExistingStudent.java
@@ -2,4 +2,8 @@ package FinalExam_EnrollmentSystem;
 
 public class ExistingStudent extends Student{
     
+    public ExistingStudent(String name, String id, double gpa, int yearLevel){
+        super(name,id,gpa,yearLevel);
+
+    }
 }
--- FinalExam_EnrollmentSystem/NewStudent.java
+++ FinalExam_EnrollmentSystem/NewStudent.java
@@ -1,5 +1,9 @@
 package FinalExam_EnrollmentSystem;
 
 public class NewStudent extends Student{
+
+    public NewStudent(String name, String id, double gpa, int yearLevel){
+        super(name,id,gpa,yearLevel);
+    }   
     
 }
